c1d7e04063c50aeaedb61602522a5a28954eb4e2
breekru <breekru@gmail.com>
2025-11-24 16:22:41 -0600

    delete function updates

--- delete_category.php
+++ delete_category.php
@@ -1,11 +1,11 @@
 <?php
-$dataPath = "data/bookmarks.json";
+$dataPath = __DIR__ . "/data/bookmarks.json";
 $board = json_decode(file_get_contents($dataPath), true);
 
-$index = $_POST['index'];
+$catIndex = $_POST['cat'] ?? null;
 
-if (isset($board['categories'][$index])) {
-    array_splice($board['categories'], $index, 1);
+if (isset($board['categories'][$catIndex])) {
+    array_splice($board['categories'], $catIndex, 1);
     file_put_contents($dataPath, json_encode($board, JSON_PRETTY_PRINT));
 }
 ?>
--- delete_link.php
+++ delete_link.php
@@ -1,12 +1,12 @@
 <?php
-$dataPath = "data/bookmarks.json";
+$dataPath = __DIR__ . "/data/bookmarks.json";
 $board = json_decode(file_get_contents($dataPath), true);
 
-$cat = $_POST['cat'];
-$index = $_POST['index'];
+$catIndex = $_POST['cat'] ?? null;
+$linkIndex = $_POST['link'] ?? null;
 
-if (isset($board['categories'][$cat]['links'][$index])) {
-    array_splice($board['categories'][$cat]['links'], $index, 1);
+if (isset($board['categories'][$catIndex]['links'][$linkIndex])) {
+    array_splice($board['categories'][$catIndex]['links'], $linkIndex, 1);
     file_put_contents($dataPath, json_encode($board, JSON_PRETTY_PRINT));
 }
 ?>
